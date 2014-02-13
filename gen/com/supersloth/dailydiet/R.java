25dc858781ea77e8f8e2c0b5ac364a5c760ce777
hchoy21 <hchoy21@gmail.com>
2014-02-13 10:34:12 -0500

    added main activity, removed random files

--- gen/com/supersloth/dailydiet/R.java
+++ gen/com/supersloth/dailydiet/R.java
@@ -13,31 +13,11 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
-    public static final class id {
-        public static final int item_detail=0x7f060002;
-        public static final int item_detail_container=0x7f060000;
-        public static final int item_list=0x7f060001;
-    }
     public static final class layout {
-        public static final int activity_item_detail=0x7f030000;
-        /** 
-    Layout alias to replace the single-pane version of the layout with a
-    two-pane version on Large screens.
-
-    For more on layout aliases, see:
-    http://developer.android.com/training/multiscreen/screensizes.html#TaskUseAliasFilters
-    
-
-    Layout alias to replace the single-pane version of the layout with a
-    two-pane version on Large screens.
-
-    For more on layout aliases, see:
-    http://developer.android.com/training/multiscreen/screensizes.html#TaskUseAliasFilters
-    
-         */
-        public static final int activity_item_list=0x7f030001;
-        public static final int activity_item_twopane=0x7f030002;
-        public static final int fragment_item_detail=0x7f030003;
+        public static final int activity_main=0x7f030000;
+    }
+    public static final class menu {
+        public static final int main=0x7f060000;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
