a52f554585b160f393364b4259190c91cf9bfc3d
hchoy21 <hchoy21@gmail.com>
2014-02-25 10:15:52 -0500

    fixed aapt.exe error

--- gen/com/supersloth/dailydiet/R.java
+++ gen/com/supersloth/dailydiet/R.java
@@ -13,15 +13,32 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
+    public static final class id {
+        public static final int action_settings=0x7f070006;
+        public static final int bFavoriteDish=0x7f070002;
+        public static final int bGroceryList=0x7f070001;
+        public static final int bHungry=0x7f070003;
+        public static final int bProfile=0x7f070004;
+        public static final int textView1=0x7f070000;
+        public static final int tvCalendarDietMain=0x7f070005;
+    }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_grocerylist=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int activity_profile=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f060000;
     }
     public static final class string {
+        public static final int action_settings=0x7f040001;
         public static final int app_name=0x7f040000;
-        public static final int title_item_detail=0x7f040001;
+        public static final int calendarDiet=0x7f040007;
+        public static final int favoriteDishes=0x7f040004;
+        public static final int groceryList=0x7f040006;
+        public static final int imHungry=0x7f040005;
+        public static final int title_item_detail=0x7f040002;
+        public static final int yourProfile=0x7f040003;
     }
     public static final class style {
         /** 
