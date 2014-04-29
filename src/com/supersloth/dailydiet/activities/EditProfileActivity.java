9757ef07f149cace61b21baad5cee84d322e8568
Hendrik <hchoy21@gmail.com>
2014-04-29 00:17:19 -0400

    jk not final commit. error in EditProfileActivity

--- src/com/supersloth/dailydiet/activities/EditProfileActivity.java
+++ src/com/supersloth/dailydiet/activities/EditProfileActivity.java
@@ -34,6 +34,15 @@ public class EditProfileActivity extends Activity {
 
 		Button save = (Button) findViewById(R.id.bSaveProfile);
 
+		SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0); 
+		name.setText(settings.getString("name", "Super Sloth")); 
+        age.setText(String.valueOf(settings.getInt("age", 21))); 
+        height.setText(String.valueOf(settings.getInt("height", 68))); 
+        weight.setText(String.valueOf(settings.getInt("weight", 175))); 
+        goal.setText(String.valueOf(settings.getInt("goal", 150))); 
+        progress.setText(String.valueOf(settings.getInt("progress", 160))); 
+		
+		
 		// when button is clicked
 		save.setOnClickListener(new View.OnClickListener() {
 
@@ -45,33 +54,46 @@ public class EditProfileActivity extends Activity {
 
 				// call SharedPreferences editor
 				SharedPreferences.Editor editor = settings.edit();
+				
+				
+				/** CRASH BELOW **/
+				if(name == null || age == null || height == null
+						|| weight == null || goal == null || progress == null){
+					
+					Toast toast = Toast.makeText(getApplicationContext(), "Please fill in all fields before proceeding", Toast.LENGTH_SHORT);
+					toast.show();
+				}
+				else if (name != null && age != null && height != null
+						&& weight != null && goal != null && progress != null) {
+					// put editText values into SharedPreferences file
+					editor.putString("name", name.getText().toString());
+					editor.putInt("age",
+							Integer.parseInt(age.getText().toString()));
+					editor.putInt("height",
+							Integer.parseInt(height.getText().toString()));
+					editor.putInt("weight",
+							Integer.parseInt(weight.getText().toString()));
+					editor.putInt("goal",
+							Integer.parseInt(goal.getText().toString()));
+					editor.putInt("progress",
+							Integer.parseInt(progress.getText().toString()));
 
-				// put editText values into SharedPreferences file
-				editor.putString("name", name.getText().toString());
-				editor.putInt("age", Integer.parseInt(age.getText().toString()));
-				editor.putInt("height",
-						Integer.parseInt(height.getText().toString()));
-				editor.putInt("weight",
-						Integer.parseInt(weight.getText().toString()));
-				editor.putInt("goal",
-						Integer.parseInt(goal.getText().toString()));
-				editor.putInt("progress",
-						Integer.parseInt(progress.getText().toString()));
+					// commit information to SharedPreferences
+					editor.commit();
 
-				// commit information to SharedPreferences
-				editor.commit();
+					// let the user know the the profile has been saved
+					String msg = "Profile Saved!";
+					Toast.makeText(getApplicationContext(), msg,
+							Toast.LENGTH_LONG).show();
 
-				// let the user know the the profile has been saved
-				String msg = "Profile Saved!";
-				Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG)
-						.show();
-
-				// create an intent to send SharedPreferences to next activity
-				// and call next activity
-				Intent intent = new Intent(getApplicationContext(),
-						ProfileActivity.class);
-				startActivity(intent);
-
+					// create an intent to send SharedPreferences to next
+					// activity
+					// and call next activity
+					Intent intent = new Intent(getApplicationContext(),
+							ProfileActivity.class);
+					startActivity(intent);
+				}
+				/** CRASH ABOVE **/
 			}
 		});
 
